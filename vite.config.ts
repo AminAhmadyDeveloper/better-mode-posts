3d299b2944d3018c2032b72ea6eba831295ec14c
Amin Ahmady <aminahmadydeveloper@gmail.com>
2024-11-15 15:41:32 -0800

    feat: auth handle done

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import { resolve } from "path";
+import vercel from "vite-plugin-vercel";
 
 export default defineConfig({
   resolve: {
@@ -8,7 +9,7 @@ export default defineConfig({
       "@": resolve(__dirname, "src"),
     },
   },
-  plugins: [react()],
+  plugins: [react(), vercel()],
   ssr: {
     noExternal: ["react-router-dom", "@apollo/client"],
   },
